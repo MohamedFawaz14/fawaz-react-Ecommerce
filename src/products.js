f0fe666d332f9eca1dfb63da70fbd2da6976e3c3
MOHAMED FAWAZ <155258376+MohamedFawaz14@users.noreply.github.com>
2024-06-26 17:06:58 +0530

    Update products.js

--- src/products.js
+++ src/products.js
@@ -2,7 +2,7 @@ const products = [
     {
         id:1,
         type:"electronics",
-        image: "public/images/Apple2023MacBookPro.png",
+        image: "../public/images/Apple2023MacBookPro.png",
         description:"Apple 2023 Macbook Pro",
         price:899
     },
